52c04d03ace92ef31e0b7cc91901a34a7104b8fa
Adam Alenius <adam.alenius@konsult.folksam.se>
2019-11-06 07:12:10 +0100

    feat:
    Update the header with new links to contact and images. Also add styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,63 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  padding: 1.45rem 1.0875rem;
+  color: black;
+  align-self: center;
+  padding-top: 10px;
+  transition: 0.25s;
+
+  :hover {
+    color: lightblue;
+  }
+`
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `gray`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: 1000,
+        display: `flex`,
+        flexDirection: `row`,
+        justifyContent: "space-between",
+        alignItems: "baseline",
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1
+        style={{
+          margin: 0,
+          fontFamily: `Limelight, serif`,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
-      <h2><Link to='/contact/' style={{ color: 'white' }}>Kontakt</Link></h2>
+      <div style={{ display: `flex`, flexDirection: `row` }}>
+        <h2>
+          <StyledLink to="/pictures/">Bilder</StyledLink>
+        </h2>
+        <h2>
+          <StyledLink to="/contact/">Kontakt</StyledLink>
+        </h2>
+      </div>
     </div>
   </header>
 )
