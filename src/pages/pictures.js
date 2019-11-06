5d35269b43f5df395216538314cf87509c3bb834
Adam Alenius <adam.alenius@konsult.folksam.se>
2019-11-06 07:14:15 +0100

    feat (pictures):
    add basic text content

---
+++ src/pages/pictures.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Img from "gatsby-image"
+
+const PicturePage = () => {
+  return (
+    <Layout>
+      <SEO title="Bilder"></SEO>
+      <h1>Bilder</h1>
+    </Layout>
+  )
+}
+
+export default PicturePage
