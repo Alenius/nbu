d63c7ddd5738ec7c0650a69f18e3223086044fae
Adam Alenius <adam.alenius@konsult.folksam.se>
2019-11-13 20:31:45 +0100

    feat (index):
    large rework of the design

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,27 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
 
 const PageContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  padding: 0 auto;
-  width: 80%;
+  flex-direction: column;
+  padding: 5rem 0;
+  align-items: center;
 `
 const MainContainer = styled.div`
   display: flex;
-  flex: 4;
   flex-direction: column;
-  align-items: center;
+  max-width: 50%;
 `
 const TextContainer = styled.div`
   display: flex;
@@ -27,41 +35,43 @@ const TextContainer = styled.div`
 `
 
 const MainTitle = styled.div`
-  font-size: 1.75rem;
-  padding-bottom: 1.5rem;
+  font-family: "Oswald", "Open-Sans", "sans-serif";
+  align-self: center;
+  font-size: 2.5rem;
+  padding-bottom: 4rem;
+  animation: ${fadeIn} 1s ease-in-out;
+  font-style: italic;
 `
 
 const SubTitle = styled.div`
-  font-size: 1.2rem;
-  align-self: flex-start;
-  padding-bottom: 0.5rem;
+  font-family: "Oswald", "Open-Sans", "sans-serif";
+  font-size: 1.5rem;
+  padding-bottom: 1rem;
+  opacity: 0;
+  animation: ${fadeIn} 1s ease-in-out forwards;
+  animation-delay: 0.25s;
 `
 
 const MainText = styled.div`
   font-size: 1rem;
-  padding-bottom: 1.5rem;
-  align-self: flex-start;
+  padding-bottom: 2rem;
+  opacity: 0;
+  animation: ${fadeIn} 1s ease-in-out forwards;
+  animation-delay: 0.5s;
+`
+const StyledLink = styled(Link)`
+  font-size: 1.1rem;
+  text-decoration: none;
+  color: #226db1;
+  opacity: 0;
+  padding-bottom: 2rem;
+  animation: ${fadeIn} 1s ease-in-out forwards;
+  animation-delay: 0.5s;
 `
 
-const SideContainer = styled.div`
-  flex: 1;
-  background-color: white;
-  padding: 1rem;
-  border-radius: 10px;
-  background-color: rgba(0, 0, 0, 0.25);
-  box-shadow: 0px 1px 1px 1px gray;
-`
-
-const SideTitle = styled.div`
-  color: white;
-  text-decoration: underline;
-  font-size: 1.2rem;
-  padding-bottom: 0.8rem;
-`
-
-const SideInfo = styled.div`
-  color: white;
-  padding-bottom: 0.8rem;
+const StyledList = styled.ul`
+  padding: 0.5rem;
+  padding-bottom: 0;
 `
 
 const IndexPage = () => (
@@ -69,30 +79,39 @@ const IndexPage = () => (
     <SEO title="Hem" />
     <PageContainer>
       <MainContainer>
-        <TextContainer>
-          <MainTitle>Din snickare i Umeå</MainTitle>
-          <MainText>
-            Vi är ett byggföretag som bryr sig om kundernas bästa.
-          </MainText>
-          <SubTitle>Bred kompetens</SubTitle>
-          <MainText>
-            Vi kan hjälpa er med det mesta inom bygg. Nybyggnation, ROT-jobb,
-            kakling, grunder, yttertak med mera.
-          </MainText>
-        </TextContainer>
+        <MainTitle>Din snickare i Umeå</MainTitle>
+        <SubTitle>Efter dina behov</SubTitle>
+        <MainText>
+          Vi kan hjälpa er med det mesta inom bygg, till exempel:
+          <StyledList>
+            <li>Nybyggnation</li>
+            <li>ROT-jobb</li>
+            <li>Murning</li>
+            <li>Kakling</li>
+            <li>Fönsterbyte</li>
+          </StyledList>
+          För att få mer uppfattning om vårat arbete så kan ni gå till vårat
+          bildgalleri där vi har bilder från jobb som vi utfört. Vi fixar det
+          mesta.
+        </MainText>
+        <StyledLink
+          to="/pictures/"
+          activeStyle={{ color: "#226db1", textDecoration: "underline" }}
+        >
+          {"Bildgalleri \u2192"}
+        </StyledLink>
+        <SubTitle>Lång erfarenhet</SubTitle>
+        <MainText>
+          Vi har funnits sedan 2005 och haft många nöjda kunder under dessa år.
+          Företaget består av två personer, Urban Nilsson och hans son David.
+        </MainText>
+        <SubTitle>Kontakt</SubTitle>
+        <MainText>
+          Om du har några frågor eller vill ha en offert så kontakta urban på
+          telefonnummer 0723 123 123, eller om du föredrar att maila på
+          urban@nilssonsbyggumea.se
+        </MainText>
       </MainContainer>
-      <SideContainer>
-        <SideTitle>Om oss</SideTitle>
-        <SideInfo>
-          Nilssons bygg Umeå startades 2005 och jobbar i Umeå med omnejd. Det
-          finns två anställda på företaget, Urban och hans son David.
-        </SideInfo>
-        <SideTitle>Kontakt</SideTitle>
-        <SideInfo>
-          Om ni undrar någonting eller vill ha en offert, ring Urban på
-          telefonnummer: 0732 121 212
-        </SideInfo>
-      </SideContainer>
     </PageContainer>
   </Layout>
 )
